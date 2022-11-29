0dc13eb3a6864630f25ea25ee04a7868ee9e644d
Andriy Demeshko <ande777complaint@gmail.com>
2022-11-29 17:13:14 +0200

    Change field years_of_experience in model Cook and create superuser

--- kitchen/models.py
+++ kitchen/models.py
@@ -14,7 +14,7 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.PositiveIntegerField()
+    years_of_experience = models.PositiveIntegerField(null=True)
 
     class Meta:
         verbose_name = "cook"
