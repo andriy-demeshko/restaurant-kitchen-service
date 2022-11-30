426a6837cac07f8a26e88abb8ca66e3b1fbb7570
Andriy Demeshko <ande777complaint@gmail.com>
2022-11-30 17:18:29 +0200

    Customize admin panel

--- kitchen/models.py
+++ kitchen/models.py
@@ -14,7 +14,7 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.PositiveIntegerField(null=True)
+    years_of_experience = models.PositiveIntegerField(null=True, blank=True)
 
     class Meta:
         ordering = ["last_name"]
