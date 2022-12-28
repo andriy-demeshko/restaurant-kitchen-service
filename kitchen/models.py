9590289c1c00b52bd7b9c998b91e28cd86b88954
Andriy Demeshko <ande777complaint@gmail.com>
2022-12-28 13:38:17 +0200

    Create ListView for sidebar menu

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        ordering = ["name"]
+        ordering = ["id"]
 
     def __str__(self):
         return self.name
@@ -22,16 +23,15 @@ class Cook(AbstractUser):
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={'pk': self.pk})
+
 
 class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    dish_type = models.ForeignKey(
-        DishType,
-        on_delete=models.CASCADE,
-        related_name="dishes"
-    )
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
 
     class Meta:
--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,60 @@
 from django.urls import path
-from kitchen.views import index
+from kitchen.views import (
+    index,
+    DishListView,
+    DishDetailView,
+    # DishCreateView,
+    # DishUpdateView,
+    DishDeleteView,
+    CookListView,
+    CookDetailView,
+    # CookCreateView,
+    # CookExperienceUpdateView,
+    CookDeleteView,
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+    # toggle_assign_to_car,
+)
 
 
 urlpatterns = [
     path("", index, name="index"),
+
+    path("dishes-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    # path(
+    #     "manufacturers/create/",
+    #     ManufacturerCreateView.as_view(),
+    #     name="manufacturer-create",
+    # ),
+    # path(
+    #     "manufacturers/<int:pk>/update/",
+    #     ManufacturerUpdateView.as_view(),
+    #     name="manufacturer-update",
+    # ),
+    # path(
+    #     "manufacturers/<int:pk>/delete/",
+    #     ManufacturerDeleteView.as_view(),
+    #     name="manufacturer-delete",
+    # ),
+
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    # path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    # path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    # path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    # path("cars/<int:pk>/toggle-assign/", toggle_assign_to_car, name="toggle-car-assign"),
+
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+    # path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    # path(
+    #     "drivers/<int:pk>/update/",
+    #     DriverLicenseUpdateView.as_view(),
+    #     name="driver-update",
+    # ),
+    # path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
 ]
 
 app_name = "kitchen"
