0e6540d653fa5d870e1380817fbaa6454f051d0f
Andriy Demeshko <ande777complaint@gmail.com>
2023-01-25 15:41:09 +0200

    Remove secret key from settings.py and use .env file for this

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv, find_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-)(&(v2f1&dpune6b525)^(((x1u-@-rma_9i1$dx#nor9@b=tm")
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
